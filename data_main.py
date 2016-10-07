e1bfd75cfe78e4e924edd0f1691393dcad68907d
Sam Toyer <sam@qxcv.net>
2016-10-07 15:34:48 +1100

    Remove useless files

--- data_main.py
+++
@@ -1,6 +0,0 @@
-import hdf5
-
-def main():
-    
-if __name__ == '__main__':
-    main()
--- dggs/create_dggs.py
+++
@@ -1,8 +0,0 @@
-from rhealpix_dggs.ellipsoids import WGS84_ELLIPSOID
-
-
-def setup_dggs(ellipsoid, north_sq, south_sq, side):
-    dggs = RHEALPixDGGS(ellipsoid, north_sq, south_sq, side)
-    return dggs
-
- 
